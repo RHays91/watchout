0114b07559668560fbcd79221f58338ca19e7e98
RHays91 <rhaysvm42@gmail.com>
2015-07-10 15:31:42 -0700

    implement game board with moving enemies

--- client/watchout.js
+++ client/watchout.js
@@ -1,2 +1,22 @@
 // start slingin' some d3 here.
+var width = 800;
+var height = 500;
+var data = [1,2,3,4,5,6,7,8,9,10];
+var svg = d3.select("body").append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
+var enemies = svg.selectAll("circle")
+  .data(data)
+  .enter().append("circle")
+  .attr("r", 10)
+  .attr("cx", function(){return Math.random()* width;})
+  .attr("cy", function(){return Math.random()*height;});
+
+var moveEnemies = function(enemies){
+  enemies.transition()
+    .attr("cx", function(){return Math.random()*width;})
+    .attr("cy", function(){return Math.random()*height;})
+};
+
+setInterval(function(){moveEnemies(enemies);}, 1000);
