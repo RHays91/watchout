577052a5b55a1e977c5ef105c58d6057ebe0e453
RHays91 <rhaysvm42@gmail.com>
2015-07-10 15:51:52 -0700

    build player svg element

--- client/watchout.js
+++ client/watchout.js
@@ -1,13 +1,15 @@
 // start slingin' some d3 here.
 var width = 800;
 var height = 500;
-var data = [1,2,3,4,5,6,7,8,9,10];
+var data = [1,2,3,4];
+
 var svg = d3.select("body").append("svg")
   .attr("width", width)
   .attr("height", height);
 
 var enemies = svg.selectAll("circle")
   .data(data)
+  .attr('class', 'enemy')
   .enter().append("circle")
   .attr("r", 10)
   .attr("cx", function(){return Math.random()* width;})
@@ -18,5 +20,12 @@ var moveEnemies = function(enemies){
     .attr("cx", function(){return Math.random()*width;})
     .attr("cy", function(){return Math.random()*height;})
 };
+
+var player = svg.append('circle')
+  .attr('class', 'player')
+  .attr("r", 10)
+  .attr("cx", function(){return Math.random()*width;})
+  .attr("cy", function(){return Math.random()*height;})
+  .attr('fill', 'red');
 
 setInterval(function(){moveEnemies(enemies);}, 1000);
